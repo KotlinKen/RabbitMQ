11b296c28507d1941600fbfb972c3d5e1bcc2d34
kotlinken@gmail.com <kotlinken@gmail.com>
2020-05-28 18:47:28 +0900

    server

--- src/main/java/com/pure/RabbitMqApplication.java
+++ src/main/java/com/pure/RabbitMqApplication.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
+//@EnableScheduling
 public class RabbitMqApplication {
 	
 	@Profile("usage_message")
